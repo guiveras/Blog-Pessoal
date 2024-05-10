43ac87ad89664f991a35534c0f72341977d2571a
Guilherme <guiverao2@gmail.com>
2024-05-10 13:19:12 -0300

    first commit

---
+++ jjblog/post/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from.models import Post, Comment
+from.forms import CommentForm # type: ignore
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    comments = Comment.objects.filter(post=post).order_by('-created_at')
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'post_detail.html', {'post': post, 'comments': comments, 'form': form})
+        
